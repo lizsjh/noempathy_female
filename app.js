66fa6f3cbc00b3039eaac293c5193852bc277c72
lizsjh <37032480+lizsjh@users.noreply.github.com>
2024-01-17 17:24:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ botui.message.add({
         delay:4000,
         loading: true,
         photo: true,
-        content:'I can help you with that. Could you tell me your order number below?'
+        content:'I can help you with that. First, could you tell me your order number?'
     });
 }).then(function(){
     return botui.action.text({
@@ -45,21 +45,52 @@ botui.message.add({
         delay:2000,
         loading: true,
         photo: true,
-        content:'Alright. Please give me a moment'
-    });
-}).then(function(){
-    return botui.message.add({
-        delay:4000,
-        loading: true,
-        photo: true,
-        content:'I checked your order. There has been a system error, and no driver was assigned to your order.'
+        content:'Alright. Please give me a moment while pulling up your order.'
     });
 }).then(function(){
     return botui.message.add({
         delay:5000,
         loading: true,
         photo: true,
-        content:'I found the nearest driver, and your food can be picked up within five minutes. Would you like to proceed with your order? If not, we can cancel your order as well.'
+        content:'You have ordered one pair of jeans, one swimsuit, and one necklace. Can you confirm?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:2000,
+        loading: true,
+        photo: true,
+        content:'Oh, in fact, you ordered a different set of products.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:2000,
+        loading: true,
+        photo: true,
+        content:'I accidentally pulled up the wrong order.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:2000,
+        loading: true,
+        photo: true,
+        content:'I am searching for your order again. Please hold on.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:30000,
+        loading: true,
+        photo: true,
+        content:'Your order includes one pair of jeans, one sweater, and one baseball cap. Please confirm.'
     });
 }).then(function(){
     return botui.action.text({
@@ -72,17 +103,33 @@ botui.message.add({
     response.push(res.value);
 }).then(function(){
     return botui.message.add({
-        delay:30000,
+        delay:2000,
         loading: true,
         photo: true,
-        content:'It is taking longer than usual because I am still gathering some information to process your request.'
+        content:'Which item is missing?'
     });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+        });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
 }).then(function(){
     return botui.message.add({
         delay:4000,
         loading: true,
         photo: true,
-        content:'Let me keep trying to process your request. Please hold on for a moment.'
+        content:'I can create a new order for the missing item to be delivered within a day. Please give me a moment.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:30000,
+        loading: true,
+        photo: true,
+        content:'It is taking longer than usual because I am still gathering some information to process your request. Please hold on again.'
     });
 }).then(function(){
     return botui.message.add({
@@ -94,7 +141,7 @@ botui.message.add({
 }).then(function(){
     sendcomplete();
     return botui.message.add({
-        delay:3000,
+        delay:2000,
         loading: true,
         photo: true,
         content:'Please contact us again if you need further assistance. Bye.'
@@ -102,5 +149,5 @@ botui.message.add({
 });
 
 function sendcomplete(){
-    window.parent.postMessage({"message": "completed","text1":response[0],"text2":response[1],"text3":response[2]}, "*");
+    window.parent.postMessage({"message": "completed","text1":response[0],"text2":response[1],"text3":response[2], "text4":response[3], "text5":response[4]}, "*");
 };
